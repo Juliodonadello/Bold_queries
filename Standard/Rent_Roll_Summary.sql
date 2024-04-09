68140c4be136307a6a48554ffc136d07ccf37830
juliodonadello <juliodonadello@gmail.com>
2024-04-09 10:25:17 -0300

    deleted old rr

--- Standard/Rent_Roll_Summary.sql
+++ Standard/Rent_Roll_Summary.sql
@@ -19,20 +19,27 @@ CHARGES_TOT AS (
 		CASE WHEN CHARGE_CONTROL. "BASE_RENT" = 1 THEN "public"."lease_recurring_charge_amounts"."amount" ELSE 0 END AS "RENT_CHARGE",
   		CASE WHEN CHARGE_CONTROL. "BASE_RENT" = 0 THEN "public"."lease_recurring_charge_amounts"."amount" ELSE 0 END AS "OTHER_CHARGE",
   		"public"."lease_recurring_charge_amounts"."effective_date" AS "EFFECTIVE_DATE",
-  		"public"."units"."property_id" "PROP_ID"
+  		"public"."units"."property_id" "PROP_ID",
+  		"public"."lease_recurring_charges"."unit_id" "UNIT_ID"
   
 	FROM "public"."lease_recurring_charges"
 	LEFT OUTER JOIN "public"."lease_recurring_charge_amounts"
 		ON "public"."lease_recurring_charges"."id" = "public"."lease_recurring_charge_amounts"."recurring_charge_id"
-	INNER JOIN "public"."leases_units_units"  --aumenta los registros pero esta bien que se duplique un lease con distintas units
-  		ON "public"."lease_recurring_charges"."lease_id" ="public"."leases_units_units"."leasesId"
+	--INNER JOIN "public"."leases_units_units"  --aumenta los registros pero esta bien que se duplique un lease con distintas units
+  	--	ON "public"."lease_recurring_charges"."lease_id" ="public"."leases_units_units"."leasesId"
  	 INNER JOIN "public"."units"
-  		ON "public"."leases_units_units"."unitsId" =  "public"."units"."id"
+  		--ON "public"."leases_units_units"."unitsId" =  "public"."units"."id"
+  		ON "public"."lease_recurring_charges"."unit_id" =  "public"."units"."id"
   	INNER JOIN CHARGE_CONTROL
   		ON CHARGE_CONTROL. "PROP_ID" = "public"."units"."property_id"
   		AND CHARGE_CONTROL. "ITEM_ID" = "public"."lease_recurring_charges"."order_entry_item_id"
   
   	WHERE "public"."lease_recurring_charge_amounts"."effective_date" <= @AsOfDate
+	AND (
+		"public"."lease_recurring_charge_amounts"."deleted_at" >= @AsOfDate 
+		OR 
+		"public"."lease_recurring_charge_amounts"."deleted_at" IS NULL
+		)
 	AND (
 		"public"."lease_recurring_charge_amounts"."frequency" != 'One Time' --not a one time charge
 		OR
@@ -55,6 +62,14 @@ CHARGES AS (
  INNER JOIN MAX_CHARGES
  	ON CHARGES_TOT."RCHARGE_ID" =  MAX_CHARGES."RCHARGE_ID" 
 	AND CHARGES_TOT."EFFECTIVE_DATE" =  MAX_CHARGES."EFFECTIVE_DATE"
+  GROUP BY 
+  CHARGES_TOT."RCHARGE_ID",
+  CHARGES_TOT."LEASE_ID",
+  CHARGES_TOT."RENT_CHARGE",
+  CHARGES_TOT."OTHER_CHARGE",
+  CHARGES_TOT."EFFECTIVE_DATE",
+  CHARGES_TOT."PROP_ID",
+  CHARGES_TOT."UNIT_ID"
 	),
 LEASES AS (
   SELECT "public"."leases"."id" AS "LEASE_ID",
@@ -80,7 +95,6 @@ LEASES AS (
   			OR ("public"."leases"."start" <= @AsOfDate AND "public"."leases"."end" IS NULL)
 		)
 		AND "public"."leases"."status" = 'current'
-
   	
   GROUP BY
   		"public"."leases"."id",
@@ -110,6 +124,7 @@ LEASES_CHARGES AS (
 	FROM LEASES
 	LEFT JOIN CHARGES
 		ON  LEASES."LEASE_ID" = CHARGES."LEASE_ID"
+  		AND LEASES."UNIT_ID" = CHARGES."UNIT_ID"
 	GROUP BY LEASES."LEASE_ID",
 		LEASES."UNIT_ID",
 		LEASES."TENANT" 
@@ -160,11 +175,11 @@ FINAL AS (
 		"LEASE_STATUS",
 		"DEPOSIT",
 		"REFUNDABLE",
-	"RENT_CHARGE",
+		"RENT_CHARGE",
 		"OTHER_CHARGE"
 	from LEASES_CHARGES
 	group by 
-	"LEASE_ID",
+		"LEASE_ID",
 		"UNIT_ID",
 		"lease_created_at", 
 		"start",
@@ -173,7 +188,7 @@ FINAL AS (
 		"LEASE_STATUS",
 		"DEPOSIT",
 		"REFUNDABLE",
-	"RENT_CHARGE",
+		"RENT_CHARGE",
 		"OTHER_CHARGE"
 	ORDER BY LEASES_CHARGES."LEASE_ID"
 	),
@@ -184,7 +199,9 @@ FINAL_AUX AS (
     "UNIT_ID"
     FROM FINAL
     GROUP BY "UNIT_ID"
-)
+	)
+
+
 SELECT 
 	UNITS."PROP_ID",
 	UNITS."PROP_NAME",
