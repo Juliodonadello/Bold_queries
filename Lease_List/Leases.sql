e415c655b3947b7f25735ce334fa38014dbccf54
juliodonadello <juliodonadello@gmail.com>
2024-07-10 16:58:31 -0300

    lease list backup

---
+++ Lease_List/Leases.sql
@@ -0,0 +1,21 @@
+SELECT "public"."leases"."id",
+	"public"."leases"."name",
+	"public"."leases"."status",
+	"public"."leases"."start",
+	"public"."leases"."end",
+	"public"."units"."name" AS "units_name",
+	"public"."units"."city",
+	"public"."units"."total_square_footage",
+	"public"."tenants"."name" AS "tenants_name",
+	"public"."properties"."name" AS "properties_name"
+
+FROM "public"."leases"
+INNER  JOIN "public"."leases_units_units" ON "public"."leases"."id"="public"."leases_units_units"."leasesId"
+INNER  JOIN "public"."units" ON "public"."units"."id"="public"."leases_units_units"."unitsId"
+INNER  JOIN "public"."tenants" ON "public"."tenants"."id"="public"."leases"."primaryTenantId"
+INNER  JOIN "public"."properties" ON "public"."units"."property_id"="public"."properties"."id"
+INNER  JOIN "public"."company_accounts" ON "public"."properties"."company_relation_id"="public"."company_accounts"."id"
+
+WHERE  CAST("public"."properties"."company_relation_id" AS INT) = CAST(@REAL_COMPANY_ID AS INT)
+    AND "public"."properties"."name" IN (@Property_Name)
+	AND  CAST("public"."leases"."status" AS TEXT) IN (@Lease_Status)
