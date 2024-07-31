ef94ff24f1690a8589c1cee38c00dac0c03b385b
juliodonadello <juliodonadello@gmail.com>
2024-07-31 15:43:36 -0300

    fix

---
+++ Lease_Clauses/tricc_imp/Lease_Status_parameter.sql
@@ -0,0 +1,11 @@
+SELECT "public"."leases"."status" "LEASE_STATUS"
+
+FROM "public"."leases"
+INNER  JOIN "public"."leases_units_units" ON "public"."leases"."id"="public"."leases_units_units"."leasesId"
+INNER  JOIN "public"."units" ON "public"."units"."id"="public"."leases_units_units"."unitsId"
+INNER  JOIN "public"."tenants" ON "public"."tenants"."id"="public"."leases"."primaryTenantId"
+INNER  JOIN "public"."properties" ON "public"."units"."property_id"="public"."properties"."id"
+
+WHERE  "public"."leases"."end" IS NOT NULL
+    AND CAST("public"."properties"."company_relation_id" AS INT)  = CAST(@REAL_COMPANY_ID AS INT)
+    AND "public"."properties"."name" IN (@Property_Name)
