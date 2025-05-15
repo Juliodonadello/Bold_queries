65fb1339cef0b4b801cfacd31291d2378119f535
juliodonadello <juliodonadello@gmail.com>
2025-05-15 17:00:31 -0300

    fix sq ft reconciliation & fix projected revenue 10 yeards

--- Square_Footage_Reconciliation/square_footage_Reconciliation.sql
+++ Square_Footage_Reconciliation/square_footage_Reconciliation.sql
@@ -27,8 +27,8 @@ WITH SQ_FT_TEMP AS (
 		AND "public"."unit_square_footage_items"."deleted_at" is null
 		
 	GROUP BY  1,3,4,5,6
-  		
 ),
+/*
 UNITS AS (
   SELECT 
 		"public"."properties"."id" AS "PROP_ID",
@@ -63,7 +63,43 @@ UNITS AS (
 		"public"."unit_square_footage_items"."square_footage_type",
 		"public"."units"."unit_class",
   		"public"."properties"."company_relation_id"
+)*/
+UNITS AS (
+  SELECT 
+    "public"."properties"."id" AS "PROP_ID",
+    "public"."properties"."name" AS "PROP_NAME",
+    "public"."units"."id" AS "UNIT_ID",
+    "public"."units"."name" AS "UNIT_NAME",
+	'Total' AS "SQ_FT_TYPE",
+	"public"."units"."unit_class" AS "UNIT_CLASS",
+	"public"."properties"."company_relation_id" as "COMPANY_ID",
+    MAX(COALESCE(uq."value", "public"."units"."total_square_footage")) AS "UNIT_SQ_FT"
+  
+  FROM "public"."units"
+  INNER JOIN "public"."properties"
+    ON "public"."units"."property_id" = "public"."properties"."id"
+  
+  LEFT JOIN (
+    SELECT DISTINCT ON ("unit_id") 
+      "unit_id",
+      "value",
+      "as_of_date"
+    FROM "public"."unit_square_footage_items"
+    WHERE "square_footage_type" = 'Total'
+      AND "as_of_date" <= @AsOfDate
+    ORDER BY "unit_id", "as_of_date" DESC
+  ) AS uq
+    ON uq."unit_id" = "public"."units"."id"
+  
+  WHERE "public"."properties"."deleted_at" IS NULL
+    AND ("public"."units"."deleted_at" >= @AsOfDate OR "public"."units"."deleted_at" IS NULL)
+    AND "public"."properties"."name" IN (@Property_Name)
+    AND CAST("public"."properties"."company_relation_id" AS INT) = CAST(@REAL_COMPANY_ID AS INT)
+	AND "public"."units"."status" = 'active'
+  
+  GROUP BY 1,2,3,4,5,6,7
 )
+
 
 SELECT 	UNITS."PROP_ID",
 		UNITS."PROP_NAME",
--- Square_Footage_Reconciliation/square_footage_Reconciliation_summary.sql
+++ Square_Footage_Reconciliation/square_footage_Reconciliation_summary.sql
@@ -29,6 +29,7 @@ WITH SQ_FT_TEMP AS (
 	GROUP BY  1,3,4,5,6
   		
 ),
+/*
 UNITS AS (
   SELECT 
 		"public"."properties"."id" AS "PROP_ID",
@@ -63,6 +64,41 @@ UNITS AS (
 		"public"."unit_square_footage_items"."square_footage_type",
 		"public"."units"."unit_class",
   		"public"."properties"."company_relation_id"
+)*/
+UNITS AS (
+  SELECT 
+    "public"."properties"."id" AS "PROP_ID",
+    "public"."properties"."name" AS "PROP_NAME",
+    "public"."units"."id" AS "UNIT_ID",
+    "public"."units"."name" AS "UNIT_NAME",
+	'Total' AS "SQ_FT_TYPE",
+	"public"."units"."unit_class" AS "UNIT_CLASS",
+	"public"."properties"."company_relation_id" as "COMPANY_ID",
+    MAX(COALESCE(uq."value", "public"."units"."total_square_footage")) AS "UNIT_SQ_FT"
+  
+  FROM "public"."units"
+  INNER JOIN "public"."properties"
+    ON "public"."units"."property_id" = "public"."properties"."id"
+  
+  LEFT JOIN (
+    SELECT DISTINCT ON ("unit_id") 
+      "unit_id",
+      "value",
+      "as_of_date"
+    FROM "public"."unit_square_footage_items"
+    WHERE "square_footage_type" = 'Total'
+      AND "as_of_date" <= @AsOfDate
+    ORDER BY "unit_id", "as_of_date" DESC
+  ) AS uq
+    ON uq."unit_id" = "public"."units"."id"
+  
+  WHERE "public"."properties"."deleted_at" IS NULL
+    AND ("public"."units"."deleted_at" >= @AsOfDate OR "public"."units"."deleted_at" IS NULL)
+    AND "public"."properties"."name" IN (@Property_Name)
+    AND CAST("public"."properties"."company_relation_id" AS INT) = CAST(@REAL_COMPANY_ID AS INT)
+	AND "public"."units"."status" = 'active'
+  
+  GROUP BY 1,2,3,4,5,6,7
 )
 
 SELECT 	UNITS."PROP_ID",
@@ -73,10 +109,16 @@ SELECT 	UNITS."PROP_ID",
 		SUM(UNITS."UNIT_SQ_FT") "sum_units_TOT_SQ_FT",  		
 		SQ_FT_TEMP."TOT_SQ_FT" "TOT_SQ_FT",
 		SQ_FT_TEMP."PROP_SQ_FT" "PROP_SQ_FT",
+		/*
 		CASE   WHEN (SQ_FT_TEMP."TOT_SQ_FT" > SQ_FT_TEMP."PROP_SQ_FT") THEN 1 
 							WHEN (SQ_FT_TEMP."TOT_SQ_FT" < SQ_FT_TEMP."PROP_SQ_FT") THEN 1 
 							ELSE 0 
 				END AS "COUNT_PROP_DIFF_SQ_FT"
+		*/
+		CASE 
+			WHEN ABS(SQ_FT_TEMP."TOT_SQ_FT" - SQ_FT_TEMP."PROP_SQ_FT") >= 1 THEN 1
+			ELSE 0
+		END AS "COUNT_PROP_DIFF_SQ_FT"
 
 FROM UNITS
 INNER JOIN SQ_FT_TEMP
